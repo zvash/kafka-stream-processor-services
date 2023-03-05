a3d9358430e390ab919bd8186eda1f3ea99d88a8
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-03-05 03:42:11 +0330

    jce encryption implemented

---
+++ config-server/src/main/java/xyz/gouril/microservices/demo/config/server/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package xyz.gouril.microservices.demo.config.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/encrypt/**")
+                .requestMatchers("/decrypt/**");
+
+    }
+
+}
